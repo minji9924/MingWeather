c6bffb6e52bb5f9ff55fce1df466b74a8b5f64b0
Minji <58546758+minji9924@users.noreply.github.com>
2020-05-31 22:47:43 +0900

    5-2

---
+++ app/src/main/java/com/pureb9924/mingweather/SettingsActivity.java
@@ -0,0 +1,14 @@
+package com.pureb9924.mingweather;
+
+import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SettingsActivity extends PreferenceActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_settings);
+    }
+}
