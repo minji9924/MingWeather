074bac01d6c6aa39ba93f77c1903544cd26a14a3
Minji <58546758+minji9924@users.noreply.github.com>
2020-05-31 22:37:12 +0900

    5-1

--- app/src/main/java/com/pureb9924/mingweather/MainActivity.java
+++ app/src/main/java/com/pureb9924/mingweather/MainActivity.java
@@ -2,6 +2,7 @@ package com.pureb9924.mingweather;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        return true;
     }
 }
